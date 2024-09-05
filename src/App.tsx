f3b56926b309962ed4b61a1b1b0b13d1359ea5ff
lixk <2664276518@qq.com>
2024-09-05 16:03:28 +0800

    chore: update

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {
   TonConnectButton,
   useIsConnectionRestored,
   useTonWallet,
+  useTonConnectUI,
 } from "@tonconnect/ui-react";
 
 const Button = () => {
@@ -17,6 +18,18 @@ const Button = () => {
 }
 
 function App() {
+  const [tonConnectUI] = useTonConnectUI();
+
+  const transaction = {
+    messages: [
+        {
+            address: "0:cd60b11355dbc5ded0d14e245fef1ba3014154ef077959e8102e10656d6ea02c", // destination address
+            amount: "20000000" //Toncoin in nanotons
+        }
+    ],
+    validUntil: 10000,
+  }
+
   return (
     <>
       <TonConnectUIProvider
@@ -28,6 +41,9 @@ function App() {
         <span>My App with React UI</span>
         <Button />
       </TonConnectUIProvider>
+      <button onClick={() => tonConnectUI.sendTransaction(transaction)}>
+          Send transaction
+      </button>
     </>
   );
 }
